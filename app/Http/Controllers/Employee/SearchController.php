b5f24166660eec92a68a5790f6dbec136e047237
Erik Justian <justin.rikz8@gmail.com>
2018-07-07 09:29:02 +0700

    search setengah jadi(sort)

--- app/Http/Controllers/Employee/SearchController.php
+++ app/Http/Controllers/Employee/SearchController.php
@@ -6,14 +6,30 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Models\JobRequest;
+use App\Models\Location;
+use App\Models\Category;
 
 class SearchController extends Controller
 {
-    public function index() {
-        $job = JobRequest::get()->where('status',JobRequest::JOB_REQUEST_STATUS_AWAITING);
-        dd($job);
-
-        return view('layouts.employee.searchjob');
+    public function index(Request $request) {
+        $job = JobRequest::where('status', JobRequest::JOB_REQUEST_STATUS_AWAITING);
+        if($request->query('category')) {
+           $job = $job->where('category_id', $request->query('category'));
+        }
+        if($request->query('location')) {
+            $job = $job->where('location_id', $request->query('location'));
+        }
+        $job = $job->where('status',JobRequest::JOB_REQUEST_STATUS_AWAITING)
+        ->paginate(2);
+        $job->load(['category', 'user']);
+        $data['jobs'] = $job;
+        // Untuk tarek data location
+        $location = Location::all();
+        $data['locations'] = $location;
+        // Untuk tarek data category
+        $category = Category::all();
+        $data['categories'] = $category;
+        return view('layouts.employee.searchjob', $data);
     }
     // Untuk mengambil job
     public function takeJob() {
